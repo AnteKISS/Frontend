c51e69c50f40539e2708c2a1eecbaf03d10b2ad4
samsim97 <samuel.simard97@hotmail.com>
2024-05-29 21:11:22 -0400

    Delete collisionManager.ts

--- src/scripts/managers/collisionManager.ts
+++
@@ -1,6 +0,0 @@
-// export class CollisionManager {
-
-//   public static checkCollision(sprite1: Phaser.GameObjects.Sprite, sprite2: Phaser.GameObjects.Sprite, callback: ArcadePhysicsCallback) {
-//     sprite1.scene.physics.add.collider(sprite1, sprite2, callback);
-//   }
-// }
