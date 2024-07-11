1024751c92aea5579d2360fcba3bbf0ad314faa0
Xelif88 <felixantoinebegin@gmail.com>
2024-07-11 14:02:12 -0400

    b

--- src/scripts/keycloak.ts
+++ src/scripts/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 export const keycloak = new Keycloak({
   url: 'http://localhost:8180/auth',
-  realm: 'Master',
+  realm: 'master',
   clientId: 'my-phaser-game'
 });
 
