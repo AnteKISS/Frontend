efdeb593ce13483baefbc43a4b62443169d00ddd
med04saad <mohammed.saad.malahi@gmail.com>
2024-07-17 22:14:59 -0400

    plus de merede qui marche cette fois

---
+++ src/scripts/scenes/mainMenu.ts
@@ -0,0 +1,61 @@
+
+import Phaser from 'phaser';
+
+
+export default class MainMenu extends Phaser.Scene {
+
+    constructor() {
+        super('MainMenu');
+    }
+
+      create() {
+        this.add.image(600, 400, 'backGround');
+        
+            // Créer les boutons
+            const playButton = this.createButton(625, 400, 'play', () => this.startGame());
+            const settingsButton = this.createButton(625, 450, 'setting', () => this.openSettings());
+            const exitButton = this.createButton(625, 500, 'exit', () => this.exitGame());
+            //this.add.rectangle(600, 300, 30, 200, 0xff4444);
+    
+    
+              
+          }
+    
+           createButton(x: number, y: number, key: string, callback: () => void) {
+              const button = this.add.image(x, y, key).setInteractive();
+          
+              button.setTint(0xff0000);
+          
+              button.on('pointerout', () => {
+                button.setTint(0xff0000);
+              });
+          
+              button.on('pointerdown', () => {
+                button.setTint(0x660000);
+                callback();
+              });
+          
+              button.on('pointerup', () => {
+                button.setTint(0xff0000);
+              });
+          
+              return button;
+            }
+        
+          startGame() {
+            console.log('Play button clicked');
+            this.scene.start('Pregame');
+          }
+        
+          openSettings() {
+            console.log('Settings button clicked');
+            this.scene.start('Setting');
+          }
+        
+          exitGame() {
+            console.log('Exit button clicked');
+            //fermer tous ???
+          }
+    
+
+}
---
+++ src/scripts/scenes/setting.ts
@@ -0,0 +1,102 @@
+
+import Phaser from 'phaser';
+
+
+export default class Setting extends Phaser.Scene {
+
+    TintColorClicked: number = 0x660000;
+    TintColorUnclicked: number = 0xff0000;
+    muteClicked: boolean = false;
+    volume: number = 50;
+    tempVolume;number = 0;
+
+    constructor() {
+        super('Setting');
+    }
+
+    create() {
+    this.add.image(600, 400, 'backGround');
+    
+    // Créer les boutons
+    const soundupButton = this.createButton(500, 400, 'plus', () => this.increaseSound());
+    const sounddownButton = this.createButton(600, 400, 'moins', () => this.decreaseSound());
+    const muteButton = this.createButtonMute(700, 400, 'mute', () => this.mute());
+    const returnButton = this.createButton(60,60 , 'return', () => this.return());
+    //bouton retour
+    }
+    
+    createButton(x: number, y: number, key: string, callback: () => void) {
+        const button = this.add.image(x, y, key).setInteractive();
+    
+        button.setTint(this.TintColorUnclicked);
+    
+        button.on('pointerout', () => {
+        button.setTint(this.TintColorUnclicked);
+        });
+    
+        button.on('pointerdown', () => {
+        button.setTint(this.TintColorClicked);
+        callback();
+        });
+    
+        button.on('pointerup', () => {
+        button.setTint(this.TintColorUnclicked);
+        });
+    
+        return button;
+    }
+
+    createButtonMute(x: number, y: number, key: string, callback: () => void) {
+        const button = this.add.image(x, y, key).setInteractive();
+    
+        
+        if(!this.muteClicked){
+            button.setTint(this.TintColorUnclicked);
+
+            button.on('pointerdown', () => {
+            button.setTint(this.TintColorClicked);
+            callback();
+            });
+        
+            button.on('pointerup', () => {
+            this.muteClicked = true;
+            button.setTint(this.TintColorUnclicked);
+            });
+        }
+        else if(this.muteClicked){
+            button.setTint(this.TintColorClicked);
+    
+            button.on('pointerdown', () => {
+            button.setTint(this.TintColorUnclicked);
+            callback();
+            });
+        
+            button.on('pointerup', () => {
+            this.muteClicked = false;
+            button.setTint(this.TintColorClicked);
+            });
+        
+        }
+    
+        return button;
+    }
+        
+    increaseSound() {
+    console.log('increaseSound button clicked');
+    // increase sound by 5
+    }
+        
+    decreaseSound() {
+    console.log('decreaseSound button clicked');
+    //decrease soune by 5
+    }
+
+    mute() {
+    console.log('mute button clicked');
+
+    }
+    return() {
+        console.log('mute button clicked');
+        this.scene.start('MainMenu');
+        }
+}
