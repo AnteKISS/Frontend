710799163cfba03e5075703ec9445078fbce2696
samsim97 <samuel.simard97@hotmail.com>
2024-07-31 23:18:27 -0400

    Added new stat from db into monster mapper

--- src/scripts/mappers/MonsterEntityMapper.ts
+++ src/scripts/mappers/MonsterEntityMapper.ts
@@ -31,6 +31,12 @@ export class MonsterEntityMapper {
           monsterData.totalModifierStats.maxHealth = stat.statValue || monsterData.totalModifierStats.maxHealth;
           monsterData.dynamicStats.health = stat.statValue || monsterData.dynamicStats.health;
           break;
+        case 'MANA':
+          monsterData.baseModifierStats.maxMana = stat.statValue || monsterData.baseModifierStats.maxMana;
+          monsterData.tempModifierStats.maxMana = stat.statValue || monsterData.tempModifierStats.maxMana;
+          monsterData.totalModifierStats.maxMana = stat.statValue || monsterData.totalModifierStats.maxMana;
+          monsterData.dynamicStats.mana = stat.statValue || monsterData.dynamicStats.mana;
+          break;
         case 'MOV_SPEED':
           monsterData.baseModifierStats.baseMovementSpeed = stat.statValue || monsterData.baseModifierStats.baseMovementSpeed;
           monsterData.baseModifierStats.movementSpeed = stat.statValue || monsterData.baseModifierStats.movementSpeed;
@@ -44,10 +50,25 @@ export class MonsterEntityMapper {
           monsterData.tempModifierStats.attackSpeed = stat.statValue || monsterData.tempModifierStats.attackSpeed
           monsterData.totalModifierStats.attackSpeed = stat.statValue || monsterData.totalModifierStats.attackSpeed;
           break;
+        case 'MELEE_RANGE':
+          monsterData.baseModifierStats.meleeRange = stat.statValue || monsterData.baseModifierStats.meleeRange;
+          monsterData.tempModifierStats.meleeRange = stat.statValue || monsterData.tempModifierStats.meleeRange;
+          monsterData.totalModifierStats.meleeRange = stat.statValue || monsterData.totalModifierStats.meleeRange;
+          break;
         case 'DAMAGE':
           monsterData.baseModifierStats.basePhysicalDamage = stat.statValue || monsterData.baseModifierStats.basePhysicalDamage;
           monsterData.tempModifierStats.basePhysicalDamage = stat.statValue || monsterData.tempModifierStats.basePhysicalDamage;
           monsterData.totalModifierStats.basePhysicalDamage = stat.statValue || monsterData.totalModifierStats.basePhysicalDamage;
+          break;
+        case 'DEFENSE':
+          monsterData.baseModifierStats.defense = stat.statValue || monsterData.baseModifierStats.defense;
+          monsterData.tempModifierStats.defense = stat.statValue || monsterData.tempModifierStats.defense;
+          monsterData.totalModifierStats.defense = stat.statValue || monsterData.totalModifierStats.defense;
+          break;
+        case 'SIGHT_DISTANCE':
+          monsterData.baseModifierStats.sightDistance = stat.statValue || monsterData.baseModifierStats.sightDistance;
+          monsterData.tempModifierStats.sightDistance = stat.statValue || monsterData.tempModifierStats.sightDistance;
+          monsterData.totalModifierStats.sightDistance = stat.statValue || monsterData.totalModifierStats.sightDistance;
           break;
       }
     });
