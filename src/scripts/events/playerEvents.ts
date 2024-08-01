30debd65879c0b26d6918478cf68914f27110161
samsim97 <samuel.simard97@hotmail.com>
2024-07-31 22:28:44 -0400

    Update playerEvents.ts

--- src/scripts/events/playerEvents.ts
+++ src/scripts/events/playerEvents.ts
@@ -26,40 +26,4 @@ export namespace PlayerEvents {
       super(player, item);
     }
   }
-
-  // export abstract class PlayerCombatEvent {
-  //   public player: PlayerEntity;
-  //   public target: MonsterEntity;
-
-  //   constructor(player: PlayerEntity, target: MonsterEntity) {
-  //     this.player = player;
-  //     this.target = target;
-  //   }
-  // }
-
-  // export abstract class PlayerMovementEvent {
-  //   public player: PlayerEntity;
-
-  //   constructor(player: PlayerEntity) {
-  //     this.player = player;
-  //   }
-  // }
-
-  // export class PlayerStartAttack extends PlayerCombatEvent {
-  //   constructor(player: PlayerEntity, target: MonsterEntity) {
-  //     super(player, target);
-  //   }
-  // }
-
-  // export class PlayerDamaged extends PlayerCombatEvent {
-  //   constructor(player: PlayerEntity, target: MonsterEntity) {
-  //     super(player, target);
-  //   }
-  // }
-
-  // export class PlayerKilled extends PlayerCombatEvent {
-  //   constructor(player: PlayerEntity, target: MonsterEntity) {
-  //     super(player, target);
-  //   }
-  // }
 }
